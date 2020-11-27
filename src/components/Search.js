cdb0827ea81d360c192f39f583bcd78881c23a26
Ben Thompson <btomo86@gmail.com>
2020-11-27 11:40:16 +0000

    project setup and written first test

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "../styles/Search.css";
+
+const Search = () => {
+  return (
+    <>
+      <input className="search-input" type="text"></input>
+    </>
+  );
+};
+
+export default Search;
